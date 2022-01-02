7a51e4c8b803b83f2be387f24a5faeecfa284f7e
Anderson Wilson <andewils@yahoo.com>
2022-01-02 16:45:59 -0500

    Persist Redux Store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,10 +1,19 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { campsites } from './campsites';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -15,8 +24,10 @@ export const ConfigureStore = () => {
             promotions,
             favorites
         }),
-        applyMiddleware(thunk)
+        applyMiddleware(thunk, logger)
     );
 
-    return store;
-}
+    const persistor = persistStore(store);
+
+    return { persistor, store };
+};
